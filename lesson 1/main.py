47e3e00fb2fcdfb9b8697a7e581e0388ca948fb7
AndiRrahmani <andirrahmani28@gmail.com>
2026-04-02 20:46:58 +0200

    lesson 1

---
+++ lesson 1/main.py
@@ -0,0 +1,52 @@
+name= "Andi"
+surname = " Rrahmani"
+
+
+full_name = name + surname
+age= 17
+temperature= 98.2
+
+users = ["Andi","Nili","Olti","Ensar"]
+
+#print(users)
+
+#users.append("Edi")
+#print(users)
+
+#users.reverse()
+#print(users.reverse())
+
+#print(users.count("Andi"))
+
+#users.clear()
+#print(users)
+
+
+
+users = ["Andi","Nili","Olti","Ensar"]
+#print(users)
+#users.insert(2,"Donjeta")
+#print(users)
+
+users = ["Andi","Nili","Olti","Ensar"]
+print(users)
+personiFitues = users.pop()
+print(users)
+
+
+
+
+
+#print(name)
+#print(age)
+#print(temperature)
+#print(full_name)
+
+#print(type(name))
+#print(type(age))
+
+#print(users[0])
+#print(users[1])
+#print(users[2])
+#print(users[3])
+#print(users)
