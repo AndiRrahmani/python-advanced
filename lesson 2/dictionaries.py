9e79ba16b975975eb612b8a15e3ab45ce36bcaa9
AndiRrahmani <andirrahmani28@gmail.com>
2026-04-14 20:46:43 +0200

    lesson 2

---
+++ lesson 2/dictionaries.py
@@ -0,0 +1,28 @@
+numrat = {
+    "KS" : "+383",
+    "IT" : "+39",
+    "AL" : "+355"
+}
+
+Kosova = numrat["KS"]
+
+#print(Kosova)
+
+print(numrat["AL"])
+
+numrat ["KS"] = "+299"
+
+print(numrat["KS"])
+
+#me fshi ni element
+
+del numrat["AL"]
+
+print(numrat)
+
+
+
+numrat2 = numrat.keys()
+numrat3 = numrat.values()
+print(keys)
+print(vlerat)
---
+++ lesson 2/tuples.py
@@ -0,0 +1,17 @@
+#print("hello")
+
+names =["andi","nili","olti"]
+
+print (names[1])
+
+empty_tp = 0
+
+print(empty_tp)
+
+person = ("Andi", 16, "Engenier")
+
+name ,age, profesioni = person
+
+print(name)
+print(age)
+print(profesioni)
