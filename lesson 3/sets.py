9f32378ed373be78ade8668f7258fcdf01e6a510
AndiRrahmani <andirrahmani28@gmail.com>
2026-04-16 20:43:18 +0200

    lesson 3

---
+++ lesson 3/sets.py
@@ -0,0 +1,19 @@
+#my_set = {1,2,3}
+
+#print(my_set)
+
+#my_set = set([1,2,3,3,3,4])
+
+#my_set ={2,2,2,2,3,4}
+#print(my_set)
+
+set1 = {2,5,6,4}
+set2 = {2,5,9,6}
+
+rezultatiUnionit = set1.union(set2)
+rezultatiUnionit2 = set1 | set2
+
+
+
+print(rezultatiUnionit)
+print(rezultatiUnionit2)
