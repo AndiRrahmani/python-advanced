b5ebe77f70cc980cab26f3ceb8e25fbcbba34b99
AndiRrahmani <andirrahmani28@gmail.com>
2026-05-05 20:42:17 +0200

    lesson 8

---
+++ lesson 8/kerri.py
@@ -0,0 +1,19 @@
+class Kerri:
+
+    def __init__(self,emri,viti,modeli,kilometart,prejardhja):
+        self.emri = emri
+        self. viti = viti
+        self. modeli = modeli
+        self. kilometart = kilometart
+        self. prejardhja=prejardhja
+
+
+    def rriteShpejtesine(self):
+        print("shpejtesia eshte duke u rritur")
+
+
+    def ndalu(self):
+        print("stooop")
+
+    def info(self):
+        print(f"{self.emri} , eshte nje veture mjaft luksoze dhe eshte e podhuar ne bitin: {self.viti}, dhe eshte i prodhuar ne: {self.prejardhja}")
---
+++ lesson 8/main.py
@@ -0,0 +1,23 @@
+from kerri import Kerri
+from Personi import Personi
+
+kerri1 = Kerri("toyota",2019,"landcruiser",00,"japoni")
+kerri2 = Kerri("Tesla",2019,"landcruiser",00,"japoni")
+
+kerri1.ndalu()
+
+print(kerri1.emri)
+print(kerri2.emri)
+
+
+kerri1.info()
+kerri2.info()
+
+personi1 = Personi("Andi",2009,"M")
+personi2 = Personi("Nili",2011,"M")
+
+personi1.prezantimi()
+personi1.sayHi()
+
+personi2.prezantimi()
+personi2.sayHi()
