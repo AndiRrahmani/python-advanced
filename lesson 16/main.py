1e5db3a94703f9c9cfd28dcf860c39e4d4532af1
AndiRrahmani <andirrahmani28@gmail.com>
2026-06-02 20:48:01 +0200

    lesson 16

--- lesson 16/main.py
+++ lesson 16/main.py
@@ -10,5 +10,53 @@ def main():
     if st.button("click"):
         st.write("buton clicked")
 
+age = st.number_input("enter your age", min_value=0,max_value=100)
+st.write("your age is",age)
+
+message = st.text_area("enter a message")
+
+if st.button("Success"):
+    st.success("Operation was successfull")
+
 if __name__=="__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
