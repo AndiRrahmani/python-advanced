eca4de9324cdc0216f9b8a991a903d9493faa9b8
AndiRrahmani <andirrahmani28@gmail.com>
2026-06-04 20:44:11 +0200

    lesson 17

---
+++ lesson 17.2/table.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+tab1,tab2,tab3 = st.tabs(["Tab 1","Tab 2","Tab 3"])
+
+with tab1:
+    st.header("Content for Tab 1")
+    st.write("this is the content of the first tab")
+with tab2:
+        st.header("Content for Tab 2")
+        st.write("this is the content of the second tab")
+with tab3:
+            st.header("Content for Tab 2")
+            st.write("this is the content of the third tab")
